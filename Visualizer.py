30e4bd1dd165ce1e881137c7b414facc170657eb
Erik <glebellier@protonmail.com>
2021-02-16 16:35:39 +0100

    Changes in results representation

--- Visualizer.py
+++ Visualizer.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Visualizer:
     def __init__(self, list_global_sparsity, list_epochs, lists_training_loss, list_test_loss):
         super().__init__()
-        self.list_global_sparsity = list_global_sparsity  # list of all pruning amounts applied
-        self.list_epochs = list_epochs  # list of all epochs computed
-        self.lists_training_loss = lists_training_loss  # list of list of losses
-        self.list_test_loss = list_test_loss
+        self.list_global_sparsity = np.array(list_global_sparsity)  # list of all pruning amounts applied
+        self.list_epochs = np.array(list_epochs)+1  # list of all epochs computed
+        self.lists_training_loss = np.array(lists_training_loss)  # list of list of losses
+        self.list_test_loss = np.array(list_test_loss)
 
         for loss_list in self.lists_training_loss:
             if len(loss_list) > len(self.list_epochs):
@@ -27,7 +28,7 @@ class Visualizer:
         f, ax = plt.subplots(figsize=(10, 10))
         for i in range(a, b):
             list_loss = self.lists_training_loss[i]
-            ax.plot(self.list_epochs[:len(list_loss)], list_loss, label="Sp = {:.2f}".format(self.list_global_sparsity[i]))
+            ax.plot(self.list_epochs[:len(list_loss)], list_loss/np.mean(self.lists_training_loss[0]), label="Sp = {:.2f}".format(self.list_global_sparsity[i]))
         ax.set_title("Training loss on MNIST")
         ax.set_xlabel("number of epochs")
         ax.set_ylabel("Training loss")
@@ -36,9 +37,9 @@ class Visualizer:
 
     def show_score_pruning(self):
         """Plots the evolution of the last training when the pruning ratio increase"""
-        list_final_loss = [elt[-1] for elt in self.lists_training_loss]
+        list_final_loss = np.array([elt[-1] for elt in self.lists_training_loss])
         f, ax = plt.subplots(figsize=(10, 10))
-        ax.plot(self.list_global_sparsity, list_final_loss, label="loss")
+        ax.plot(self.list_global_sparsity, list_final_loss/list_final_loss[0], label="loss")
         ax.set_title("Training loss after {} epochs".format(self.list_epochs[-1]))
         ax.set_xlabel("Global Sparsity")
         ax.set_ylabel("Training loss")
@@ -47,7 +48,7 @@ class Visualizer:
 
     def show_test_loss(self):
         f, ax = plt.subplots(figsize=(10, 10))
-        ax.plot(self.list_global_sparsity, self.list_test_loss, label="loss")
+        ax.plot(self.list_global_sparsity, self.list_test_loss/self.list_test_loss[0], label="loss")# normalization by the loss of the biggest model
         ax.set_title("Test loss after {} epochs training".format(self.list_epochs[-1]))
         ax.set_xlabel("Global Sparsity")
         ax.set_ylabel("Test Loss")
