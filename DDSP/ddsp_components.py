358cf43238b3d954eafa3c940a4b828d86581be8
ChapaChapa0 <49436218+ChapaChapa0@users.noreply.github.com>
2021-02-17 04:09:41 +0100

    + noise

--- DDSP/ddsp_components.py
+++ DDSP/ddsp_components.py
@@ -29,19 +29,34 @@ def harmonic_synth(f0, total_amp, harm_amps, samplerate):
 
 
 # Subtractive Synthesizer with Noise Filtering
-def filtered_noise(noise_mag, samplerate):
+def filtered_noise(magnitudes, block_size):
 
     # Init parameters
-    batch_size, n_samples, n_filter_banks = np.shape(noise_mag)
-
+    batch_size, n_samples, n_bands = np.shape(magnitudes)
+    
     # Create noise signal
-    noise = torch.rand([batch_size, n_samples]) * 2 - 1 # noise values between -1 and 1
+    noise = (torch.rand(batch_size, n_samples, block_size)).to(magnitudes) * 2 - 1 # noise values between -1 and 1
 
-    # Compute inpulse response from magnitude
-    noise_mag_compl = torch.complex(noise_mag, torch.zeros([batch_size, n_samples, n_filter_banks]))
-    impulse_response = torch.fft.irfft(noise_mag_compl)
+    # Compute frequency impulse response
+    magnitudes = torch.stack([magnitudes, torch.zeros_like(magnitudes)], -1) # add a dimension
+    magnitudes = torch.view_as_complex(magnitudes) # transform in complex before inverse FFT
+    impulse_response = torch.fft.irfft(magnitudes)
+    
+    # Window impulse response and make it causal
+    filter_size = impulse_response.shape[-1] # [2 * n_bands - 2]
+    impulse_response = torch.roll(impulse_response, filter_size // 2, -1)
+    win = torch.hann_window(filter_size, dtype=impulse_response.dtype, device=impulse_response.device)
+    impulse_response = impulse_response * win
 
-    # Filter noise signal
-    audio = nn.Conv1d(noise, impulse_response)
+    # Zero padding before convolution
+    impulse_response = nn.functional.pad(impulse_response, (0, block_size - filter_size))
+    impulse_response = torch.roll(impulse_response, -filter_size // 2, -1)
 
-    return audio
+    # Filter noise signal with frequency impulse response
+    noise = nn.functional.pad(noise, (0, noise.shape[-1]))
+    impulse_response = nn.functional.pad(impulse_response, (impulse_response.shape[-1], 0))
+    audio = fft.irfft(fft.rfft(noise) * fft.rfft(impulse_response))
+    audio = audio[..., audio.shape[-1] // 2:].contiguous()
+    audio = audio.reshape(audio.shape[0], -1, 1)
+
+    return audio[:,:,0] # [batch_size, n_samples * block_size]
